57c78c28faff57368f1d803d244d0a210eafaed8
gkrish90 <sivaramakrishna.gullapali@botnotch.com>
2023-08-15 19:56:26 +0530

    updated configuration for deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://tejayeachuri.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/visionindustriesindia',
+  baseUrl: '/visionindustriesindia/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
