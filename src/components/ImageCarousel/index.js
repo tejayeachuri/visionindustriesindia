a17f822c5211884c4e72a145e7dbc9fbf3f80019
gkrish90 <sivaramakrishna.gullapali@botnotch.com>
2023-08-18 16:34:17 +0530

    Added carousel with buttons

--- src/components/ImageCarousel/index.js
+++ src/components/ImageCarousel/index.js
@@ -1,43 +1,114 @@
-// src/components/ImageCarousel.js
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { makeStyles } from "@material-ui/core/styles";
+import { Carousel } from 'react-carousel-minimal';
 import React from 'react';
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import './ImageCarousel.css';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import "react-multi-carousel/lib/styles.css";
+import Link from '@docusaurus/Link';
 
-const images = [
-   
-  ];
+const useStyles = makeStyles({
+  captionStyle: {
+    fontSize: '2em',
+    fontWeight: 'bold',
+  },
+  slideNumberStyle: {
+    fontSize: '20px',
+    fontWeight: 'bold',
+  },
+  carouselContainer: {
+    position: 'relative',
+  },
+  contentContainer: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    textAlign: 'center',
+    zIndex: 1,
+    color: 'white',
+  },
+  title: {
+    fontSize: '2.5em',
+    marginBottom: '10px',
+  },
+  subtitle: {
+    fontSize: '1.5em',
+    marginBottom: '20px',
+  },
+  customButton: {
+    padding: '10px 20px',
+    background: 'rgba(0, 0, 0, 0.5)',
+    color: 'white',
+    border: 'none',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    textDecoration: 'none', // Add this to remove underline
+  },
+});
 
-const ImageCarousel = () => {
-    return (
-        <Carousel autoPlay infiniteLoop className="custom-carousel" >
-                <div>
-                    <img src={useBaseUrl("/img/Image2.jpeg")} />
-                </div>
-                <div>
-                    <img src={useBaseUrl("/img/Image3.jpeg")} alt="Image 3" />
-                </div>
-                <div>
-                    <img src={useBaseUrl("/img/Image4.jpeg")} alt="Image 4" />
-                </div>
-                <div>
-                    <img src={useBaseUrl("/img/Image5.jpeg")} alt="Image 5" />
-                </div>
-                <div>
-                    <img src={useBaseUrl("/img/Image6.jpeg")} alt="Image 6" />
-                </div>
-                <div>
-                    <img src={useBaseUrl("/img/Image7.jpeg")} alt="Image 7" />
-                </div>
-                <div>
-                    <img src={useBaseUrl("/img/Image8.jpeg")} alt="Image 8" />
-                </div>
-                <div>
-                    <img src={useBaseUrl("/img/Image9.jpeg")} alt="Image 9" />
-                </div>
-        </Carousel>
-    );
+const captionStyle = {
+  fontSize: '2em',
+  fontWeight: 'bold',
 };
+
+const slideNumberStyle = {
+  fontSize: '20px',
+  fontWeight: 'bold',
+};
+
+const data = [
+  {
+    image: "https://raw.githubusercontent.com/tejayeachuri/visionindustriesindia/main/static/img/Image2.jpeg",
+  },
+  {
+    image: "https://raw.githubusercontent.com/tejayeachuri/visionindustriesindia/main/static/img/Image3.jpeg",
+
+  },
+  {
+    image: "https://raw.githubusercontent.com/tejayeachuri/visionindustriesindia/main/static/img/Image4.jpeg",
+
+  },
+  {
+    image: "https://raw.githubusercontent.com/tejayeachuri/visionindustriesindia/main/static/img/Image5.jpeg",
+
+  },
+  {
+    image: "https://raw.githubusercontent.com/tejayeachuri/visionindustriesindia/main/static/img/Image6.jpeg",
+
+  },
+];
+
+const ImageCarousel = ({ MoviesPath_Description_JSON }) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.carouselContainer}>
+      <div className={classes.contentContainer}>
+        <div className={classes.title}>Vision Industries India</div>
+        <div className={classes.subtitle}>Lets Enrich Life Together</div>
+        <Link
+          className={classes.customButton}
+          to="/docs/intro">
+          Explore More
+        </Link>
+      </div>
+      <Carousel
+        data={data}
+        time={2000}
+        width="100vw"
+        height="60vh"
+        captionStyle={captionStyle}
+        slideNumberStyle={slideNumberStyle}
+        captionPosition="bottom"
+        automatic={true}
+        dots={true}
+        pauseIconColor="white"
+        pauseIconSize="40px"
+        slideBackgroundColor="darkgrey"
+        slideImageFit="cover"
+      />
+    </div>
+  );
+}
 
 export default ImageCarousel;
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,32 +9,31 @@ import ImageCarousel from '@site/src/components/ImageCarousel';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Explore More
-          </Link>
-        </div>
-      </div>
+        <header className={clsx('hero hero--primary', styles.heroBanner)}>
+          <div className="container">
+            <h1 className="hero__title">{siteConfig.title}</h1>
+            <p className="hero__subtitle">{siteConfig.tagline}</p>
+            <div className={styles.buttons}>
+              <Link
+                className="button button--secondary button--lg"
+                to="/docs/intro">
+                Explore More
+              </Link>
+            </div>
+          </div>
     </header>
   );
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <ImageCarousel/>
+      <ImageCarousel />
       <main>
         <HomepageFeatures />
       </main>
