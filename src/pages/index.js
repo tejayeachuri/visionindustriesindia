7ba2dd383833e06a852046fe6dfe7638a8dbcd39
gkrish90 <sivaramakrishna.gullapali@botnotch.com>
2023-08-19 20:36:16 +0530

    Changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,24 +5,23 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import ImageCarousel from '@site/src/components/ImageCarousel';
-
 import styles from './index.module.css';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-        <header className={clsx('hero hero--primary', styles.heroBanner)}>
-          <div className="container">
-            <h1 className="hero__title">{siteConfig.title}</h1>
-            <p className="hero__subtitle">{siteConfig.tagline}</p>
-            <div className={styles.buttons}>
-              <Link
-                className="button button--secondary button--lg"
-                to="/docs/intro">
-                Explore More
-              </Link>
-            </div>
-          </div>
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro">
+            Explore More
+          </Link>
+        </div>
+      </div>
     </header>
   );
 }
@@ -32,7 +31,7 @@ export default function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Lets Enrich Life Together">
       <ImageCarousel />
       <main>
         <HomepageFeatures />
